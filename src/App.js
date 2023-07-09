6e8301527ea26336033db346f8052a425d885f3a
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-09 18:20:21 +0200

    Add navigation routes to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>This is the shop page</h1>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
