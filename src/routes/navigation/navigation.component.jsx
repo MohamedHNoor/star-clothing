991f5799c227f597afdf8a81b7267739b833e485
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-09 18:18:55 +0200

    Add navigation component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import { ReactComponent as StarLogo } from '../../assets/star-logo.svg';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <StarLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
