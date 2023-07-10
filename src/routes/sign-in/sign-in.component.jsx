f5ad40da215b6de1fe30167a5d922462b8ef70b0
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-10 19:58:20 +0200

    Add sign in methods

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,15 @@
+import { signInWithGooglePopUp } from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopUp();
+    console.log(response);
+  };
+
   return (
     <div>
       <h1>Sing in Page</h1>
+      <button onClick={logGoogleUser}>sign in with google popup</button>
     </div>
   );
 };
